e2359aa15b72bd056d318853712715d2d7b0140b
iampremtamang <ktamangprem@gmail.com>
2025-02-18 17:26:58 -0500

    Refactor EsewaPayment class: improved docstrings, method names, and environment handling git commit -m "Refactor EsewaPayment class: improved docstrings, method names, and environment handling
    - Updated docstrings to provide clear and consistent explanations.
    - Changed method names for clarity (e.g., create_signature).
    - Added is_dev_environment flag for better environment handling (development vs production).
    - Improved error handling and logging."

--- esewa/payment.py
+++ esewa/payment.py
@@ -8,26 +8,32 @@ from typing import Dict, Optional, Tuple
 from .signature import generate_signature
 
 class EsewaPayment:
-    '''
+    """
     A class to handle eSewa payment integration.
-    Attributes:
-    secret_key (str): Secret Key for HMAC signature generation.
-    product_code (str): Your Product Code.
-    Methods:
-    generate_signature(params):
-    create_payment_form(amount, tax_amount, total_amount, transaction_uuid, success_url, failure_url):
-    send_payment_request(amount, tax_amount, total_amount, transaction_uuid, success_url, failure_url):
-    check_transaction_status(product_code, total_amount, transaction_uuid):
     
-    '''
-    def __init__(self, product_code="EPAYTEST", success_url=None, failure_url=None, secret_key=None):
+    Attributes:
+        - secret_key (str): Secret Key for HMAC signature generation.
+        - product_code (str): Your Product Code.
+    
+    Methods:
+        - create_signature(self, total_amount: float, transaction_uuid: str):
+        - generate_form(self):
+        - get_status(self):
+        - is_completed(self):
+        - send_payment_request(amount, tax_amount, total_amount, transaction_uuid, success_url, failure_url):
+        - check_transaction_status(product_code, total_amount, transaction_uuid):
+    
+    """
+    def __init__(self, product_code="EPAYTEST", success_url=None, failure_url=None, secret_key=None, is_dev_environment=True) -> None:
         """
         Initializes the EsewaPayment instance.
 
-        :param product_code: The product code provided by eSewa.
-        :param success_url: Optional. URL to redirect upon successful payment.
-        :param failure_url: Optional. URL to redirect upon payment failure.
-        :param secret_key: Optional. Secret key for the eSewa API.
+        Args:
+            - product_code: The product code provided by eSewa.
+            - success_url: Optional. URL to redirect upon successful payment.
+            - failure_url: Optional. URL to redirect upon payment failure.
+            - secret_key: Optional. Secret key for the eSewa API.
+            - is_dev_environment: States whether it’s a development environment or production environment. Default value is True.
         """
         # Handle secret key
         if secret_key:
@@ -38,7 +44,7 @@ class EsewaPayment:
             if not hasattr(settings, 'ESEWA_SECRET_KEY'):
                 logger.warning(
                     "Using default secret key for EsewaPayment. "
-                    "Please set ESEWA_SECRET_KEY in settings."
+                    "Please set ESEWA_SECRET_KEY in settings.py."
                 )
             self.secret_key = secret_key or getattr(settings, 'ESEWA_SECRET_KEY', "8gBm/:&EnhH.1/q")
         
@@ -51,7 +57,7 @@ class EsewaPayment:
             if not hasattr(settings, 'ESEWA_SUCCESS_URL'):
                 logger.warning(
                     "Using default success URL for EsewaPayment. "
-                    "Please set ESEWA_SUCCESS_URL in settings."
+                    "Please set ESEWA_SUCCESS_URL in settings.py."
                 )
             self.success_url = success_url or getattr(settings, 'ESEWA_SUCCESS_URL', "http://localhost:8000/success/")
         
@@ -64,23 +70,23 @@ class EsewaPayment:
             if not hasattr(settings, 'ESEWA_FAILURE_URL'):
                 logger.warning(
                     "Using default failure URL for EsewaPayment. "
-                    "Please set ESEWA_FAILURE_URL in settings."
+                    "Please set ESEWA_FAILURE_URL in settings.py."
                 )
             self.failure_url = failure_url or getattr(settings, 'ESEWA_FAILURE_URL', "http://localhost:8000/failure/")
+        
         self.product_code = product_code
-
+        self.is_dev_environment = is_dev_environment
 
     
-    def create_signature(
-            self, 
-            total_amount: float, 
-            transaction_uuid: str
-            ) -> str:
+    def create_signature(self, total_amount: float, transaction_uuid: str) -> str:
         """
         Generates HMAC-SHA256 signature for eSewa payment gateway.
-        :param total_amount: The total amount to be paid.
-        :param transaction_uuid: A unique identifier for the transaction.
-        :return: The generated signature.
+        
+        Args:
+            - total_amount: The total amount to be paid.
+            - transaction_uuid: A unique identifier for the transaction.
+        
+        Return: The generated signature.
         """
         self.amount = total_amount
         self.uuid = transaction_uuid
@@ -97,9 +103,11 @@ class EsewaPayment:
     def simulate_payment():
         pass
 
-    def generate_form(self)->Dict[str, str]:
+    def generate_form(self) -> str:
         """
-        generate HTML code for eSewa payment gateway
+        Generates HTML form inputs for eSewa payment gateway.
+        
+        Return: generated form as a string value
         """
         payload = {
             "amount": self.amount,
@@ -122,18 +130,21 @@ class EsewaPayment:
         return form
 
 
-    def get_status(self, dev: bool) -> str:
+    def get_status(self) -> str:
         """
-        Retrieves the transaction status from eSewa based on the environment.
-        :param dev: If True, use the testing environment URL. If False, use the production environment URL.
-        :return: The status of the transaction as returned by the eSewa API.
+        Fetch the transaction status from eSewa, using the appropriate URL based on the environment.
+         
+        Return: Transaction status as a string.
         """
-        status_url_testing = f"https://uat.esewa.com.np/api/epay/transaction/status/?product_code={self.product_code}&total_amount={self.amount}&transaction_uuid={self.uuid}"
-        status_url_prod = f"https://epay.esewa.com.np/api/epay/transaction/status/?product_code={self.product_code}&total_amount={self.amount}&transaction_uuid={self.uuid}"
-
-        url = status_url_testing if dev else status_url_prod
-        response = requests.get(url)
-
+        # eSewa URL for local development/testing environment
+        esewa_url = f"https://uat.esewa.com.np/api/epay/transaction/status/?product_code={self.product_code}&total_amount={self.amount}&transaction_uuid={self.uuid}"
+        
+        # Check if the production environment is enabled.
+        if not self.is_dev_environment:
+            # eSewa Production environment URL 
+            esewa_url = f"https://epay.esewa.com.np/api/epay/transaction/status/?product_code={self.product_code}&total_amount={self.amount}&transaction_uuid={self.uuid}"
+        
+        response = requests.get(esewa_url)
         if response.status_code != 200:
             raise requests.exceptions.RequestException(f"Error fetching status: {response.text}")
 
@@ -141,42 +152,37 @@ class EsewaPayment:
         return response_data.get("status", "UNKNOWN")
 
 
-    def is_completed(self, dev: bool) -> bool:
+    def is_completed(self) -> bool:
         """
         Checks if the transaction is completed.
-        :param dev: Use the testing environment if True, production otherwise.
-        :return: True if the transaction is completed, False otherwise.
-        """
-        return self.get_status(dev) == "COMPLETE"
 
-    def __eq__(self, value: object) -> bool:
+        Return: True if transaction status value is "COMPLETE" otherwise False.
+        """
+        return self.get_status() == "COMPLETE"
+
+    def __eq__(self, instance: object) -> bool:
         """
         Compare this EsewaPayment instance with another instance for equality.
 
         Args:
-            value (object): The object to compare with.
+            - instance (object): The object to compare with.
 
-        Returns:
-            bool: True if the given object is an instance of EsewaPayment and has the same
+        Return: True if the given object is an instance of EsewaPayment and has the same
                 secret_key and product_code as this instance, False otherwise.
         """
-        ''''''
-        if not isinstance(value, EsewaPayment):
+        if not isinstance(instance, EsewaPayment):
             return False
-        return self.secret_key == value.secret_key and self.product_code == value.product_code
+        return self.secret_key == instance.secret_key and self.product_code == instance.product_code
         
-    def verify_signature(
-            self,
-        response_body_base64: str,
-    ) -> Tuple[bool, Optional[Dict[str, str]]]:
+    def verify_signature(self, response_body_base64: str) -> Tuple[bool, Optional[Dict[str, str]]]:
         """
         Verifies the signature of an eSewa response.
         
         Args:
-            response_body_base64 (str): The Base64-encoded response body.
+            - response_body_base64 (str): The Base64-encoded response body.
         
         Returns:
-            Tuple[bool, Optional[Dict[str, str]]]: 
+            - Tuple[bool, Optional[Dict[str, str]]]: 
                 A tuple where the first element is a boolean indicating the validity of the signature,
                 and the second element is a dictionary of the decoded response data if the signature is valid, otherwise None.
         """
@@ -212,14 +218,14 @@ class EsewaPayment:
 
 
 if __name__ == "__main__":
-    payment = EsewaPayment()
+    payment = EsewaPayment(is_dev_environment=True)
     signature = payment.create_signature(100, "11-201-13")
     print(f"Generated Signature: {signature}")
     payload = payment.generate_form()
     print(f"Generated Payload: {payload}")
-    status = payment.get_status(dev=True)
+    status = payment.get_status()
     print(f"Transaction Status: {status}")
-    completed = payment.is_completed(dev=True)
+    completed = payment.is_completed()
     print(f"Transaction Completed: {completed}")
     verified, response_data = payment.verify_signature("eyJ0cmFuc2FjdGlvbl9jb2RlIjoiMExENUNFSCIsInN0YXR1cyI6IkNPTVBMRVRFIiwidG90YWxfYW1vdW50IjoiMSwwMDAuMCIsInRyYW5zYWN0aW9uX3V1aWQiOiIyNDA2MTMtMTM0MjMxIiwicHJvZHVjdF9jb2RlIjoiTlAtRVMtQUJISVNIRUstRVBBWSIsInNpZ25lZF9maWVsZF9uYW1lcyI6InRyYW5zYWN0aW9uX2NvZGUsc3RhdHVzLHRvdGFsX2Ftb3VudCx0cmFuc2FjdGlvbl91dWlkLHByb2R1Y3RfY29kZSxzaWduZWRfZmllbGRfbmFtZXMiLCJzaWduYXR1cmUiOiJNcHd5MFRGbEhxcEpqRlVER2ljKzIybWRvZW5JVFQrQ2N6MUxDNjFxTUFjPSJ9 ")
 
